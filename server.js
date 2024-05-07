158fe35a790b75b4d5cc8eb43a30cdd884233b84
MIlanG95 <mgajic21234@gmail.com>
2024-05-07 20:36:50 +0200

    js changes

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/download", (req, res) => {
+    const filePath = path.join(__dirname, 'static', 'files', 'Milan.pdf');
+    res.download(filePath);
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
