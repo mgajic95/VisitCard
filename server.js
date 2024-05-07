aaa1c57015c0d086fb72c605c8d48938704fcbaa
MIlanG95 <mgajic21234@gmail.com>
2024-05-07 22:04:00 +0200

    changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,  'index.html'));
+    res.sendFile(path.join(__dirname, 'templates', 'index.html'));
 });
 
 app.get("/download", (req, res) => {
