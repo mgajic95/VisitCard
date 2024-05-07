ece1d1b4edf1b255e5dfee1f690ce92b19947192
MIlanG95 <mgajic21234@gmail.com>
2024-05-07 21:18:35 +0200

    changes

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, 'templates', 'index.html'));
 });
 
 app.get("/download", (req, res) => {
@@ -12,7 +19,18 @@ app.get("/download", (req, res) => {
     res.download(filePath);
 });
 
-const PORT = process.env.PORT || 5001;
+
+app.get('/static/assets/css/main.css', (req, res) => {
+    res.set('Content-Type', 'text/css');
+    res.sendFile(path.join(__dirname, 'public', 'static', 'assets', 'css', 'main.css'));
+});
+
+
+app.get('/static/images/avatar.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'static', 'images', 'avatar.jpg'));
+});
+
+const PORT = process.env.PORT || 5003;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
